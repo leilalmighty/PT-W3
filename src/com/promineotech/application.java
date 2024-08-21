0a50f653de320db0212e3ae1e342b4e7c7f4bc88
leilalmighty <kjohn95@icloud.com>
2024-08-21 15:51:57 -0700

    first commit being made

---
+++ src/com/promineotech/application.java
@@ -0,0 +1,40 @@
+package com.promineotech;
+
+public class application {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//operator performs actions on operands
+		// + - * /
+		int age = 34 * 2;
+				System.out.println(age);
+		boolean isAge30 = age == 30;
+		System.out.println(isAge30);
+		
+		System.out.println(age == 68);
+		
+		age = age + 1;
+		System.out.println(age);	
+		
+		String firstName = "Jodi";
+		String lastName = "Smith";
+		String fullName = firstName + ' ' + lastName;
+		System.out.println(fullName);
+		
+		int answer1 = 6 % 4;
+				System.out.println(answer1);
+		
+				int x = 25;
+				int y = 20;
+				int z = 10;
+
+				boolean result = x > y && x > z;
+				System.out.println(result);
+				
+				
+			}
+
+}
+
+
---
+++ src/com/promineotech/application2.java
@@ -0,0 +1,58 @@
+package com.promineotech;
+
+public class application2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("My first Java Program!");
+		// is how you make a comment
+
+		/*
+		 *primitive
+		 *int - an integer (whole number)
+		 *double Decimal Places (32.8465)
+		 *float - precision decimal data type (just more precise
+		 *long - like an int, numbers can be higher
+		 *short - like an int, less of a number range
+		 *byte - 8 bits of data (small)
+		 *char - a single character ( a, b, m, z, of even a space)
+		 *boolean - true or false
+		 *
+		 *objects
+		 * String - textual data, a string of characters (makes up text)
+		 */
+		// variable declaration - type (of variable), identifier or name, assignment operator (=), value (what's being assigned to identifier), semicolon
+		int leilsAge = 21;
+		double accountBalance = 120000.57;
+		char middleInitial = 'L';
+		boolean isHotOutside = true;
+		String firstName = "Kaleil";
+		System.out.println(leilsAge);
+	System.out.println(accountBalance);
+	System.out.println(middleInitial);
+	System.out.println(isHotOutside);
+	System.out.println(firstName);
+	
+	
+	
+	// == equality operator
+	System.out.println(2 == 2 );
+	System.out.println(3 > 9);
+	System.out.println(8 >= 6);
+	// && means and 
+	// || means or
+	System.out.println(2 == 2 && 1 < 5 );
+	System.out.println(true && false);
+		
+	
+	  for (int i = 1; i <= 10; i ++) {
+          if (i % 2 == 0) {
+              System.out.print(i + 1);
+          }
+
+
+		
+
+
+
+
